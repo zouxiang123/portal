a479922ff9eea278bc1fa2023d4661c42ddb8260
ysl <abc@DESKTOP-MQVGE41>
2016-12-03 16:44:16 +0800

    相差天数

--- portal-platform/src/main/java/com/xtt/platform/util/time/DateUtil.java
+++ portal-platform/src/main/java/com/xtt/platform/util/time/DateUtil.java
@@ -94,4 +94,30 @@ public class DateUtil extends DateUtils {
 		int end = (int) (endDate.getTime() / 1000);
 		return end - start;
 	}
+	/** 
+     * 计算两个日期之间相差的天数 
+     * @param moreDate 	大日期
+     * @param lessDate 	小日期
+     * @return 
+     * 	-1	日期为null或日期传入相反
+     */
+	public static int getDays(Date moreDate,Date lessDate){
+		if(moreDate == null || lessDate == null){
+			return -1;
+		}
+		
+		Calendar cal = Calendar.getInstance();  
+		cal.setTime(moreDate);  
+		long time1 = cal.getTimeInMillis();               
+		cal.setTime(lessDate);  
+		long time2 = cal.getTimeInMillis();       
+		long between_days=(time1-time2)/(1000*3600*24);  
+		return Integer.parseInt(String.valueOf(between_days));         
+	}
+	
+	public static void main(String[] args) {
+		Date d1 = new Date();
+		Date d2 = DateFormatUtil.convertStrToDate("2016-10-01", "yyyy-MM-dd");
+		System.out.println(getDays(d1, d2));
+	}
 }
