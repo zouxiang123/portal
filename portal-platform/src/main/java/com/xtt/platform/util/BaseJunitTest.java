c26f4327742b9aae217290ef5a2fa956eab4443a
swb <bruce@puma>
2017-02-09 16:40:20 +0800

    解决Junit启动BUG

--- portal-platform/src/main/java/com/xtt/platform/util/BaseJunitTest.java
+++ portal-platform/src/main/java/com/xtt/platform/util/BaseJunitTest.java
@@ -1,6 +1,5 @@
 package com.xtt.platform.util;
 
-
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -8,11 +7,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * 
  * @Description 单元测试基础类
- * @author QUINN
+ * @author Tik
  * @date 2014年8月7日下午7:20:38
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/config/springContext.xml" })
 public class BaseJunitTest {
 
+	static {
+		System.setProperty("BASE_PATH", System.getProperty("os.name").toLowerCase().startsWith("win") ? "c:/xtt" : "/home/publish/xtt");
+	}
 }
